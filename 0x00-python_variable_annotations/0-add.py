d99b8933359b4821e41701df293da468bfeddcc2
Reganmatics <devreganmatics@gmail.com>
2023-01-05 08:52:03 +0100

    add task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Task 0. variable annotations
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    A type-annoted function
+    args:
+        a -> float
+        b -> float
+
+    return: float
+    """
+    return a + b
