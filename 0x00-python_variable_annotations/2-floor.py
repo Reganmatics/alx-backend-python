f8a705bf5044d52bfb7caf4cf70a6727610ef026
Reganmatics <devreganmatics@gmail.com>
2023-01-05 11:18:31 +0100

    add task 2 and its related test file

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Basic annotations - floor
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    typed-annoted function, takes float n and returns a rounded-off value
+    """
+    return math.floor(n)
