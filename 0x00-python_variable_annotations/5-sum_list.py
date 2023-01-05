5b0158005cc9e8810f69d028d52cfa17286691be
Reganmatics <devreganmatics@gmail.com>
2023-01-05 09:04:31 +0100

    add task 5 and its test file

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+task 5: Complex types - list of floats
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    type-annoted function wihich takes a list of floats and returns a float
+    args:
+    input_list -> a list of floats
+
+    return -> float
+    """
+    return sum(input_list)
