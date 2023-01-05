cd4f30cc797e80783f0d8af95fc06280845413d4
Reganmatics <devreganmatics@gmail.com>
2023-01-05 10:24:14 +0100

    add task 1 and its associated test file

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+task 1. asic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    type-annoted function, takes string args, returns a conctenated string
+    args:
+    str1 -> string
+    str2 -> string
+
+    return -> string
+    """
+    return str1 + str2
