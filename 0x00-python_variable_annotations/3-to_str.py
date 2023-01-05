273306d081c349c6648ebb8a21488783ab70ba61
Reganmatics <devreganmatics@gmail.com>
2023-01-05 11:19:04 +0100

    add task 3 and its related test file

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    type-annoted function
+    args:
+    n -> float
+    return -> string
+    """
+    return str(n)
