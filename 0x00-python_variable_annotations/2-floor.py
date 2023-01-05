6fd05d21ba719940b82e0af256630ab221fcf785
Reganmatics <devreganmatics@gmail.com>
2023-01-05 14:31:34 +0100

    ad quick fix for task 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -3,11 +3,10 @@
 """
 Basic annotations - floor
 """
-import math
 
 
 def floor(n: float) -> float:
     """
     typed-annoted function, takes float n and returns a rounded-off value
     """
-    return math.floor(n)
+    return int(n)
