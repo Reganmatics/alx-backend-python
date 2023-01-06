1b1d61e4788f3adde8063454973aac10ecc989a0
Reganmatics <devreganmatics@gmail.com>
2023-01-06 04:03:25 +0100

    add task 8 and its related test file

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,7 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 task 8. Complex types - functions
 """
 from typing import Callable
 
-def make_multiplier(multiplier: float) -> 
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    type-annoted function -> decorator function
+    args:
+        multiplier -> float
+
+    return -> multiply
+    """
+    def multiply(x :float) -> float:
+        return x * multiplier
+    return multiply
