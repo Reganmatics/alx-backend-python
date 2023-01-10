b804b93ee712fca546e4e7dd952afc3d0b4ab88b
Reganmatics <devreganmatics@gmail.com>
2023-01-10 04:01:46 +0100

    add task 4 and its associated test file

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Task 4. Tasks
+"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+def task_wait_n(n: int, max_delay: int) -> List[asyncio.Task]:
+    """
+    args:
+        n -> int
+        max_delay -> int
+
+    return -> typing.List
+    """
+    tasks = []
+    for i in range(n):
+        task = task_wait_random(max_delay)
+        tasks.append(task)
+    return tasks
